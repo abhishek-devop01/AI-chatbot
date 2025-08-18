a683d4fa63925467de9c6035760fcd7611945cfa
abhishek devop <standup787059@gmail.com>
2025-08-18 21:40:30 +0530

    fixed credential

---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require("./src/app");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/services/ai.service");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on('ai-message', async (data)=>{
+     const response = await generateResponse(data.prompt)
+     console.log(response);
+     
+  })
+});
+
+httpServer.listen(3000, () => {
+  console.log("server is running ....");
+});
